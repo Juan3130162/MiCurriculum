02deda59a5b2a3b9ca025c81869ce2323c7bd70e
Juan3130162 <151886985+Juan3130162@users.noreply.github.com>
2023-11-23 20:18:26 -0500

    Update formulario.php

---
+++ formulario.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $destinatario = "juan-8as@hotmail.es";
+    $asunto = "Oportunidad de empleo";
+
+    $cuerpoMensaje = "Nombre: $nombre\n";
+    $cuerpoMensaje .= "Email: $email\n";
+    $cuerpoMensaje .= "Mensaje: $mensaje\n";
+
+  
+    mail($destinatario, $asunto, $cuerpoMensaje);
+
+    
+    header("Location: gracias.html");
+    exit();
+}
+?>
